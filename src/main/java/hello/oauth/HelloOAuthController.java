8e3b27e2e8294c38d8e1063424ddf9053c10d7d8
Elishka Flint <elishkaf@gmail.com>
2019-05-16 07:20:01 +0100

    Remove thymeleaf templates and revert to v simple controller

--- src/main/java/hello/oauth/HelloOAuthController.java
+++ src/main/java/hello/oauth/HelloOAuthController.java
@@ -2,23 +2,19 @@ package hello.oauth;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class HelloOAuthController {
 
-    @GetMapping("/login_old")
-    public String loginForm(Model model) {
-        model.addAttribute("loginClient", new LoginClient());
+    @GetMapping("/login")
+    public String login() {
         return "login";
     }
 
-    @PostMapping("/login_old")
-    public String loginSubmit(@ModelAttribute LoginClient loginClient) {
-        return "result";
+    @PostMapping("/landing")
+    public String landing() {
+        return "landing";
     }
 
 }
